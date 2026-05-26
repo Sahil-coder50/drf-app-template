e0be705aead3bd14282cb5e0cc9638b2803cf723
Sahil-coder50 <sahilsheoran24@gmail.com>
2026-05-26 01:31:33 +0000

    Corrected apps.py

--- {{cookiecutter.app_name}}/apps.py
+++ {{cookiecutter.app_name}}/apps.py
@@ -2,6 +2,6 @@ from django.apps import AppConfig
 
 class {{ cookiecutter.app_name|capitalize }}Config(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.{{ cookiecutter.app_name }}'
+    name = 'modules.{{ cookiecutter.app_name }}'
     verbose_name = '{{ cookiecutter.app_verbose_name }}'
     
